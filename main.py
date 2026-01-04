288b821b3c941c3dcd31a5267a4357f6762d05e7
AbdelaliMouida <abdelalimouida7@gmail.com>
2026-01-03 20:29:58 -0500

    premier commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    print("Task Manager")
+    print("1. Show tasks")
+    print("2. Add task")
+    print("3. Complete task")
+    print("4. Delete task")
+    print("5. Exit")
+
+if __name__ == "__main__":
+    main()
